b515afc62cc961c3c53de77e280801d04bdf5b31
cjieouba@outlook.com <changj@southzn.com>
2021-04-02 09:01:47 +0800

    Feat: 实现轮播图组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { ScrollableTabComponent } from './components';
 import { ImageSliderComponent } from './components/image-slider/image-slider.component';
+import { HorizontalGridComponent } from './components/horizontal-grid/horizontal-grid.component';
 
 @NgModule({
   // 该模块有哪些组件
   declarations: [
     AppComponent,
     ScrollableTabComponent,
-    ImageSliderComponent
+    ImageSliderComponent,
+    HorizontalGridComponent
   ],
   // 该模块依赖哪些模块
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   // 根模块显示的组件
--- src/app/components/image-slider/image-slider.component.ts
+++ src/app/components/image-slider/image-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 
 export interface ImageSlider {
   imgUrl: string
@@ -11,14 +11,41 @@ export interface ImageSlider {
   templateUrl: './image-slider.component.html',
   styleUrls: ['./image-slider.component.css']
 })
-export class ImageSliderComponent implements OnInit {
+export class ImageSliderComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input() sliders: ImageSlider[] = []
+  @Input() sliderHeight = '160px'
+  // 轮播时间间隔
+  @Input() intervalBySeconds = 2
   // static 是否为静态，在 if 或者 for 中包含的是 false
   @ViewChild('imageSlider', { static: true }) imgSlider: ElementRef
-  constructor() { }
+  selectIndex: number = 0
+  intervalId
+
+  constructor(
+    // 依赖注入
+    private rd2: Renderer2
+  ) { }
 
   ngOnInit(): void {
     console.log(this.imgSlider)
   }
 
+  ngAfterViewInit() {
+    this.intervalId = setInterval(() => {
+      this.rd2.setProperty(this.imgSlider.nativeElement, 'scrollLeft', (this.getIndex(++ this.selectIndex) * this.imgSlider.nativeElement.scrollWidth) / this.sliders.length)
+    }, this.intervalBySeconds * 1000)
+  }
+
+  getIndex(idx: number): number {
+    return idx >= 0 ? idx % this.sliders.length : this.sliders.length - Math.abs(idx) % this.sliders.length
+  }
+
+  handleScroll(ev) {
+    const ratio =  (ev.target.scrollLeft * this.sliders.length) / ev.target.scrollWidth
+    this.selectIndex = Math.round(ratio)
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId)
+  }
 }
